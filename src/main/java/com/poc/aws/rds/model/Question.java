f8fac627cc293f7fbe8127018b52d93c807002f2
Jorge Alvarez Alvarez - EVERIS <S56768@credito.bcp.com.pe>
2018-09-09 19:05:07 -0500

    mock

--- src/main/java/com/poc/aws/rds/model/Question.java
+++ src/main/java/com/poc/aws/rds/model/Question.java
@@ -4,16 +4,11 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
 @Getter
 @Setter
-@Entity
 @Builder
 public class Question {
 
-    @Id
     private Integer id;
 
     private String description;
